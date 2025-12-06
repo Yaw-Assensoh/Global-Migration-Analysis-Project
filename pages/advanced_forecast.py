41ba8f589517f3cbcc86e184505a8ffce40f0dc8
Yaw Assensoh <chimps212@hotmail.com>
2025-12-06 14:12:20 +0100

    adding necessary requirements

--- pages/advanced_forecast.py
+++ pages/advanced_forecast.py
@@ -1,17 +1,25 @@
 # File: pages/6_📈_Advanced_Forecasting.py
 # This replaces/enhances your existing forecasting page
-import io
 import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from plotly.graph_objects import Figure
-from utils.data_loader import load_data
-from utils.helpers import create_sidebar_filters
-from utils import visualizations, data_loader, helpers
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import io
+from datetime import timedelta
+import warnings
+warnings.filterwarnings("ignore")
 from prophet import Prophet
-from datetime import datetime, timedelta
+from prophet.plot import plot_plotly
+from statsmodels.tsa.arima.model import ARIMA
+
+
+
+ 
+
+
+
 
 
 
